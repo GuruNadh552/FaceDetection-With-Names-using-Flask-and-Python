eac1581ad3952033831f8d3951c4b44d687ab371
GuruNadh552 <gurunadhpukkalla@gmail.com>
2021-05-24 18:49:08 +0530

    Face Verification Added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response,request,redirect,url_for
 from camera import Video
 
 app=Flask(__name__)
@@ -6,6 +6,20 @@ app=Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/verifyID')
+def verifyID():
+    return render_template('verifyID.html')
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+   error = None
+   if request.method == 'POST':
+      if request.form['sid'] != 'admin' or request.form['pass_w'] != 'admin':
+         error = 'Invalid username or password. Please try again!'
+      else:
+         return redirect(url_for('verifyID'))
+   return render_template('index.html', error = error)
 
 def gen(camera):
     while True:
@@ -15,7 +29,6 @@ def gen(camera):
          b'\r\n\r\n')
 
 @app.route('/video')
-
 def video():
     return Response(gen(Video()),
     mimetype='multipart/x-mixed-replace; boundary=frame')
